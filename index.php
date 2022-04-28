b8fe8d74e700cbb4fb1a29662e7f7aa21db83357
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-04-28 10:31:37 +0200

    V1.1 ( Ajout des bouttons + test ).

--- index.php
+++ index.php
@@ -1,5 +1,61 @@
-<?php
+<?php 
 
-
+    session_start();
+    $_SESSION["prénom"] = "Adrien";
+    $_SESSION["nom"] = "Bouchet";
+    $nom = $_SESSION["nom"];
+    $Suppimer = session_destroy();
+    
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include("includes/head.inc.html") ?>
+    
+<body>
+    <?php include ("includes/header.inc.html") ?>
+     
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 mt-3">
+                <nav>
+                    
+                    <a class="btn btn-outline-secondary w-100" role="button" href="index.php">Home</a>
+
+                    <?php if (isset($table)) include_once ("/includes/ul.inc.php"); ?>
+                
+                </nav>
+            </div>
+
+                <!-- <div class="col-md-9 mt-3"> -->
+
+            <section class="col-md-9 mt-3">
+                
+                <a role="button" class="btn btn-primary">Ajouter des données</a>
+
+                <?php
+                    if($_SESSION){
+                    echo 'bonjour ' . $nom;
+                    }
+                ?>
+                    
+            </section>
+                    
+            
+        </div>
+    </div>
+    
+    <?php include ("includes/ul.inc.php"); ?>
+         
+    <?php include("includes/footer.inc.html"); ?>
+    <pre><?php var_dump($_SESSION); ?>
+    </pre>
+</body>
+</html>
+
+
+
+
