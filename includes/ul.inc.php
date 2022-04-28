bba41a262d24fef151be37c581d2cf955845ce5e
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-04-28 10:32:06 +0200

    V1.0

---
+++ includes/ul.inc.php
@@ -0,0 +1,50 @@
+<!-- <nav>
+    <button type="button" class="btn btn-outline-secondary">Home
+        </button>
+        <ul>
+            <li>Debogage</li>
+            <li>Concaténation</li>
+            <li>Boucle</li>
+            <li>Fonction</li>
+            <li>Supprimer</li>
+        </ul>
+</nav> -->
+
+<div class="container">
+    <div class="d-flex p-2 bd-highlight w-25 p-3" >
+
+        <nav>
+            <button type="button" class="btn btn-outline-secondary w-100 p-2" >Home </button>
+                <ul class="list-group">
+                    <li class="list-group-item active" aria-current="true">An active item</li>
+                    <button type="button" class="list-group-item list-group-item-action">Debogage</button>
+                    <button type="button" class="list-group-item list-group-item-action">Concaténation</button>
+                    <button type="button" class="list-group-item list-group-item-action">Boucle</button>
+                    <button type="button" class="list-group-item list-group-item-action">Fonction</button>
+                    <button type="button" class="list-group-item list-group-item-action">Supprimer</button>
+                    <!-- <li class="list-group-item">Debogage</li>
+                    <li class="list-group-item">Concaténation</li>
+                    <li class="list-group-item">Boucle</li>
+                    <li class="list-group-item">Fonction</li>
+                    <li class="list-group-item">Supprimer</li> -->
+                </ul>
+        </nav>
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
